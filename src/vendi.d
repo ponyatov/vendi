b272d3370e269d6321a448c7fb12bae21d4f8de8
Dmitry Ponyatov <dponyatov@gmail.com>
2023-11-26 16:30:34 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- src/vendi.d
+++ src/vendi.d
@@ -4,6 +4,39 @@ import std.stdio;
 
 import vibe.vibe;
 
+HTTPServerSettings settings = null;
+URLRouter router = null;
+
+static this() {
+    settings = new HTTPServerSettings;
+    settings.port = 12345;
+    settings.bindAddresses = ["127.0.0.1"];
+    settings.errorPageHandler = toDelegate(&error);
+    router = new URLRouter;
+}
+
 void main(string[] args) {
     writeln(args);
+    router.get("/", &index);
+    router.get("/hello", &hello);
+    router.get("/about", &about);
+    listenHTTP(settings, router);
+    runApplication();
+}
+
+void error(HTTPServerRequest req, HTTPServerResponse res,
+        HTTPServerErrorInfo err) {
+    res.render!("error.dt", req, err);
+}
+
+void index(HTTPServerRequest req, HTTPServerResponse res) {
+    res.render!("index.dt", req);
+}
+
+void hello(HTTPServerRequest req, HTTPServerResponse res) {
+    res.writeBody("Hello, World!");
+}
+
+void about(HTTPServerRequest req, HTTPServerResponse res) {
+    res.render!("about.dt", req);
 }
